862b8cc82b74b4dbde927daee8aed0fb24304e68
und3rdg <und3rdg@gmail.com>
2020-08-07 16:27:41 +0100

    on and off on wheel up/down, added noise padding for time and coordinates.

--- autoclick.py
+++ autoclick.py
@@ -1,4 +1,49 @@
-import autopy
+#!/bin/env python3
+import autopy as ap
+import time
+import mouse
+from pynput.mouse import Listener
+import logging
+import random as r
 
-for x in range(0, 10):
-    autopy.mouse.click(button=autopy.mouse.LEFT, delay=100)
+logging.basicConfig(filename="mouse_log.txt",
+                    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
+clickOn = False
+
+
+def on_move(x, y):
+    global clickOn
+    if clickOn and pixelPadding(x, y):
+        logging.info("Mouse moved to ({0}, {1})".format(x, y))
+        ap.mouse.click(delay=randOneOrTwoMs())
+        print("click ({0}, {1})".format(x, y))
+
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        logging.info(
+            'Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+
+
+def on_scroll(x, y, dx, dy):
+    global clickOn
+    logging.info('Mouse scrolled at ({0}, {1})({2}, {3})'.format(x, y, dx, dy))
+    print(clickOn)
+    clickOn = (dy == -1)
+
+
+def pixelPadding(x, y):
+    padding = r.randrange(4) + 3
+    return x % padding == 0 and y % padding == 0
+
+
+def randOneOrTwoMs():
+    oneOrTwoMs = r.randrange(1) / 10 + 0.1
+    noise = r.randrange(99999)/1000000
+    return oneOrTwoMs + noise
+
+
+with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    listener.join()
